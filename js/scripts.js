522c02a7148e00873e3c6c6760361e602753ecdb
Altyn15 <altynnikov313@gmail.com>
2020-10-19 15:24:36 +0300

    add carousel anchor-top

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay: true,
+        margin:0,
+        nav:true,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+
+            1000:{
+                items:1
+            }
+        }
+    });
+});
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+        $('.totop a').fadeIn();
+    } else {
+        $('.totop a').fadeOut();
+    }
+});
